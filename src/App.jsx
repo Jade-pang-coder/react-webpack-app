7d1c59f2e9d03cf0cc554a366c693bec50028c4e
Jade <pangqihui52@hotmail.com>
2026-07-16 20:24:50 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+import Welcome from "./Welcome";
+
+export default function App() {
+    return (
+        <div>
+            <h1>Hello React + Webpack!</h1>
+            < Welcome />
+        </div>
+    );
+}
